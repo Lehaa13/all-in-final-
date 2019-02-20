5fc283ac6874be4d7fd8eddd9f53a99156fc1356
Bulat <yaremakadanil@gmail.com>
2019-02-20 08:35:38 +0700

    plus

--- l2/l2/Program.cs
+++ l2/l2/Program.cs
@@ -16,6 +16,7 @@ namespace l2
             Console.Write("Enter B: ");
             int b = int.Parse(Console.ReadLine());
 
+            Console.Write("A + B = {0}", a+b);
 
 
             Console.ReadKey();
