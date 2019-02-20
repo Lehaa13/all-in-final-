c8325e3a38ce3cd4e2f3ad5134485ce982ca51a6
Bulat <yaremakadanil@gmail.com>
2019-02-20 08:34:29 +0700

    first

---
+++ l2/l2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter B: ");
+            int b = int.Parse(Console.ReadLine());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
